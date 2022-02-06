96cddfd97a04b268094f6fa42ac96f16b88c1e98
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2022-02-06 22:14:07 +0330

    fixed nobitex.csv_script

--- nobitex/csv_script.py
+++ nobitex/csv_script.py
@@ -1,13 +1,9 @@
 import datetime
 import csv
+
+from django.utils.timezone import make_aware
+
 from nobitex.models import Market, Trades
-
-
-def find_market(market_name):
-    for market in Market.objects.all():
-        if market.__str__() == market_name:
-            return market
-    return None
 
 
 def run():
@@ -15,10 +11,31 @@ def run():
     with open('nobitex/data.csv') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         next(csv_reader)
+        markets = list(Market.objects.all())
+        def get_market(market_string):
+            market = next(
+                (m for m in markets if str(m) == market_string),
+                None,
+            )
+            if market is None:
+                base_asset, quote_asset = market_string.split('-')
+                market = Market.objects.create(
+                    base_asset=base_asset,
+                    quote_asset=quote_asset
+                )
+                markets.append(market)
+            return market
         all_trades = []
         for row in csv_reader:
-            new_trade = Trades(datetime.datetime.fromtimestamp(int(float(row[4]))), float(row[2]),
-                            float(row[3]), row[1], find_market(row[0]))
+            market = get_market(row[0])
+            new_trade = Trades(
+                time=make_aware(datetime.datetime.fromtimestamp(float(row[4]))),
+                price=float(row[2]),
+                volume=float(row[3]),
+                type=row[1],
+                market=market,
+            )
             all_trades.append(new_trade)
         Trades.objects.bulk_create(all_trades)
 
+run()
