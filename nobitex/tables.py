ceef533b54bbf8859621ae52b4b7f5bca67eebbf
Sorush Bahariyan <sorush.bahariyan@gmail.com>
2022-02-14 14:59:12 +0330

    Nobitex: improved chart table

--- nobitex/tables.py
+++ nobitex/tables.py
@@ -10,24 +10,27 @@ class TradesTable(tables.Table):
         empty_text = 'No trades for given market and date!!!'
 
 class ChartTable(tables.Table):
-    low = tables.Column()
-    high = tables.Column()
-    open = tables.Column()
-    close = tables.Column()
+    low = tables.Column(verbose_name='lowest price')
+    high = tables.Column(verbose_name='highest price')
+    open = tables.Column(verbose_name='opening price')
+    close = tables.Column(verbose_name='closing price')
     start_time = tables.Column()
 
+    class Meta:
+        template_name = 'django_tables2/bootstrap4.html'
+        empty_text = 'No trades for given market and date!!!'
 
     def render_low(self, value):
-        return '| %s |' % value
+        return value
 
     def render_high(self, value):
-        return '| %s |' % value
+        return value
 
     def render_open(self, value):
-        return '| %s |' % value
+        return value
 
     def render_close(self, value):
-        return '| %s |' % value
+        return value
 
     def render_start_time(self, value):
-        return '| %s |' % value
+        return value
