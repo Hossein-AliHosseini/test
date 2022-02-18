1a95c4e079b1356c11d9e7eccaf95fe95c731fa4
Hossein AliHosseini <themmdhossein@gmail.com>
2022-02-18 21:38:24 +0330

    Fetch trades OHLC data using Celery task

--- nobitex/tasks.py
+++ nobitex/tasks.py
@@ -1,10 +1,28 @@
-from celery import shared_task
-from nobitex.models import Trades
+from celery import shared_task, Celery
+from nobitex.models import Trades, Market
+from django.db.models import Min, Max, DateTimeField
+from django.db.models.functions import Trunc
 
+app = Celery('tasks', broker='redis://127.0.0.1:6379', backend='django-db')
 
 @shared_task
 def store_trades(market):
-    queryset = Trades.objects.filter(market_id=market).values()
+    queryset = Trades.objects.filter(market__id=market).values()
     file_obj = open('trades.txt', 'a')
     for query in queryset:
         file_obj.write(str(query) + "\n")
+
+
+@app.task
+def create_chart(date, market_id):
+    market = Market.objects.get(id = market_id)
+    queryset = Trades.objects.filter(time__gte=date, market=market).\
+                    annotate(start_time=Trunc('time', 'hour', output_field=DateTimeField())).order_by('start_time').\
+                        values('start_time').\
+                            annotate(low=Min('price'), high=Max('price'), open_time=Min('time'), close_time=Max('time'))
+
+    for query in queryset:
+        query['open'] = Trades.objects.filter(time=query['open_time']).first().price
+        query['close'] = Trades.objects.filter(time=query['close_time']).first().price
+
+    return queryset[:]
--- nobitex/views.py
+++ nobitex/views.py
@@ -1,15 +1,10 @@
 from django.utils.timezone import now
 from django.shortcuts import render, HttpResponse
-from django.db.models import Min, Max, DateTimeField, Count, F, Case, When, Value, CharField, Q, FloatField
-from django.db.models.functions import Trunc
 
 from nobitex.models import Trades, Market
 from nobitex.forms import TradeForm
 from nobitex.tables import TradesTable, ChartTable
-
-from datetime import timedelta
-
-import copy
+from nobitex.tasks import create_chart
 
 
 def hello_world(request):
@@ -49,13 +44,7 @@ def candlestick_charts(request):
             'date': date,
             'market': market,
         })
-    time_step = 1
-    queryset = Trades.objects.filter(time__gte=date, market=market).\
-        annotate(start_time=Trunc('time', 'hour', output_field=DateTimeField())).order_by('start_time').\
-            values('start_time').\
-                annotate(low=Min('price'), high=Max('price'), open=Min('time'), close=Max('time'))
-                    # annotate(open=Case(When(open_time=F('time'), then=F('price')), output_field=FloatField()),
-                    #         close=Case(When(close_time=F('time'), then=F('price')), output_field=FloatField()))
+    queryset = create_chart.delay(date.strftime("%Y-%m-%d %H:%M:%S"), market.id).get()
     table = ChartTable(queryset)
     table.paginate(page=request.GET.get('page', 1), per_page=50)
     return render(request, 'candlestick_charts.html', {
