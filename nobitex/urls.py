76062eb8dd870443722b9323e818f6bb8f1c98ed
Hossein AliHosseini <themmdhossein@gmail.com>
2022-02-20 18:59:20 +0330

    Add AJAX support to render trades OHLC-like Table

--- nobitex/urls.py
+++ nobitex/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from nobitex.views import view_trades, hello_world, candlestick_charts
+from nobitex.views import view_trades, hello_world, candlestick_charts, check_status
 
 urlpatterns = [
     path('', hello_world),
     path('trades', view_trades),
     path('charts', candlestick_charts),
+    path('check_status', check_status, name='check_status'),
 ]
--- nobitex/views.py
+++ nobitex/views.py
@@ -1,10 +1,15 @@
 from django.utils.timezone import now
 from django.shortcuts import render, HttpResponse
+from django.http import JsonResponse
 
 from nobitex.models import Trades, Market
 from nobitex.forms import TradeForm
 from nobitex.tables import TradesTable, ChartTable
 from nobitex.tasks import create_chart
+
+from celery.result import AsyncResult
+
+import json
 
 
 def hello_world(request):
@@ -44,10 +49,21 @@ def candlestick_charts(request):
             'date': date,
             'market': market,
         })
-    queryset = create_chart.delay(date.strftime("%Y-%m-%d %H:%M:%S"), market.id).get()
-    table = ChartTable(queryset)
+    queryset = create_chart.delay(date.strftime("%Y-%m-%d %H:%M:%S"), market.id)
+    table = ChartTable({})
     table.paginate(page=request.GET.get('page', 1), per_page=50)
     return render(request, 'candlestick_charts.html', {
         'form': form,
         'table': table,
+        'task_id': queryset.task_id,
     })
+
+
+def check_status(request):
+    task_id = request.GET.get('task_id')
+    status = AsyncResult(task_id)
+    if status.ready():
+        print('blp')
+        return JsonResponse(status.get(), safe=False)
+    else:
+        return False
