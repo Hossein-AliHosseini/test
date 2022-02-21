8151e938ba3034e6b822d8b443994da8a9d4279f
Hossein AliHosseini <themmdhossein@gmail.com>
2022-02-21 15:19:21 +0330

    Debug tasks.create_chart

--- nobitex/tasks.py
+++ nobitex/tasks.py
@@ -16,14 +16,18 @@ def store_trades(market):
 
 @shared_task
 def create_chart(date, market_id):
-    market = Market.objects.get(id = market_id)
-    queryset = Trades.objects.filter(time__gte=date, market=market).\
-                    annotate(start_time=Trunc('time', 'hour', output_field=DateTimeField())).order_by('start_time').\
-                        values('start_time').\
-                            annotate(low=Min('price'), high=Max('price'), open_time=Min('time'), close_time=Max('time'))
+    market = Market.objects.get(id=market_id)
+    queryset = (Trades.objects.filter(time__gte=date, market=market).
+                annotate(start_time=Trunc('time', 'hour',
+                                          output_field=DateTimeField())).
+                order_by('start_time').values('start_time').
+                annotate(low=Min('price'), high=Max('price'),
+                         open_time=Min('time'), close_time=Max('time')))
 
     for query in queryset:
-        query['open'] = Trades.objects.filter(time=query['open_time']).first().price
-        query['close'] = Trades.objects.filter(time=query['close_time']).first().price
+        query['open'] = (Trades.objects.filter(time=query['open_time'],
+                                               market=market).first().price,)
+        query['close'] = (Trades.objects.filter(time=query['close_time'],
+                                                market=market).first().price)
 
     return queryset[:]
