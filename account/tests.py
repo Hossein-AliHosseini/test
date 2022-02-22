6c39bcbc455c0e69faba439b7fb21cf1f95e1d7b
Hossein AliHosseini <themmdhossein@gmail.com>
2022-02-23 02:16:11 +0330

    Add unittest to accout app

--- account/tests.py
+++ account/tests.py
@@ -1,3 +1,40 @@
+from django.contrib.auth import get_user_model
 from django.test import TestCase
 
-# Create your tests here.
+
+class UsersManagersTests(TestCase):
+
+    def test_create_user(self):
+        User = get_user_model()
+        User.objects.create_user(email='normal@user.com', password='foo')
+        user = User.objects.get(email='normal@user.com')
+        self.assertEqual(user.email, 'normal@user.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        try:
+            self.assertIsNone(user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(TypeError):
+            User.objects.create_user()
+        with self.assertRaises(TypeError):
+            User.objects.create_user(email='')
+        with self.assertRaises(ValueError):
+            User.objects.create_user(email='', password="foo")
+
+    def test_create_superuser(self):
+        User = get_user_model()
+        User.objects.create_superuser(email='super@user.com', password='foo')
+        super_user = User.objects.get(email='super@user.com')
+        self.assertEqual(super_user.email, 'super@user.com')
+        self.assertTrue(super_user.is_active)
+        self.assertTrue(super_user.is_staff)
+        self.assertTrue(super_user.is_superuser)
+        try:
+            self.assertIsNone(super_user.username)
+        except AttributeError:
+            pass
+        with self.assertRaises(ValueError):
+            User.objects.create_superuser(
+                email='super@user.com', password='foo', is_superuser=False)
