11ffd6cbc9b8e646232cd8a9877ba18802b077f8
Hossein AliHosseini <themmdhossein@gmail.com>
2022-02-26 21:59:52 +0330

    Add change password view to project

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import home, SignUp, login_view, log_out
+from .views import home, SignUp, login_view, log_out, change_password
 
 urlpatterns = [
     path('', home, name="home"),
     path("signup/", SignUp.as_view(), name="signup"),
     path('login/', login_view, name='login'),
-    path('logout/', log_out, name='log_out')
+    path('logout/', log_out, name='log_out'),
+    path('changepassword/', change_password, name='change_password'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.forms import (UserCreationForm,
+                                       AuthenticationForm,
+                                       PasswordChangeForm)
 from django.views.generic.edit import CreateView
-from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth import login, logout, update_session_auth_hash
 from django.http import HttpResponseRedirect
+from django.contrib import messages
 
 
 def home(request):
@@ -27,6 +30,24 @@ def login_view(request):
 def log_out(request):
     logout(request)
     return HttpResponseRedirect('/home')
+
+
+def change_password(request):
+    if request.method == 'POST':
+        form = PasswordChangeForm(request.user, request.POST)
+        if form.is_valid():
+            user = form.save()
+            update_session_auth_hash(request, user)
+            messages.success(request,
+                             'Your password was successfully updated!')
+            return redirect('change_password')
+        else:
+            messages.error(request, 'Please correct the error below.')
+    else:
+        form = PasswordChangeForm(request.user)
+    return render(request, 'registration/change_password.html', {
+        'form': form
+    })
 
 
 class SignUp(CreateView):
