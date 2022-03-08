30354e8040283a0ece874110d6bd033dfb70ed3f
Hossein AliHosseini <themmdhossein@gmail.com>
2022-03-08 16:52:07 +0330

    Add dropbox menu for chat and change loggedin and anonymus users permissions

--- chat/views.py
+++ chat/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.contrib.contenttypes.models import ContentType
+from django.contrib.auth.decorators import login_required
 
 from chat.models import Message
 
 
+@login_required(login_url='signup')
 def index(request):
-    return render(request, 'index.html')
+    user = request.user
+    return render(request, 'index.html', {'user': user})
 
 
 def room(request, room_name):
